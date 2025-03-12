e9e6145583fbf2e18d1854f7a21620a2d6154ef4
Elhassan Abouyousouf <elhassanabouyousouf@Elhassans-MacBook-Pro.local>
2025-03-12 11:49:58 +0100

    changed the airport to airports

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -23,7 +23,7 @@ SELECT o.city AS origin_city,
 		d.country AS dest_country,
 		f.*
 FROM flight_route_stats f
-LEFT JOIN {{ref('prep_airport')}} o
+LEFT JOIN {{ref('prep_airports')}} o
 	ON f.origin=o.faa
-LEFT JOIN {{ref('prep_airport')}} d
+LEFT JOIN {{ref('prep_airports')}} d
 	ON f.dest=d.faa
